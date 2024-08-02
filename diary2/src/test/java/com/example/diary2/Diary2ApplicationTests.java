7f2889310a0ed5302d66b1d04e28a705700f2f50
sonhyunjae@naver.com <sonhyunjae9553@naver.com>
2024-08-02 19:35:25 +0900

    test

---
+++ diary2/src/test/java/com/example/diary2/Diary2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diary2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Diary2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
